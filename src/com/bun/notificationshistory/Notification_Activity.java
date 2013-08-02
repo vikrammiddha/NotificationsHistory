0f146a34d03b829df1a248883fde1eeca1448343
vikrammiddha <vikram.middha@makepositive.com>
2013-08-03 00:50:29 +0530

    Changed tracker/history name reference to Spy

--- src/com/bun/notificationshistory/Notification_Activity.java
+++ src/com/bun/notificationshistory/Notification_Activity.java
@@ -592,7 +592,7 @@ public class Notification_Activity extends Activity{
 		        Notification_Activity.this);
 
 		// Setting Dialog Title
-		alertDialog2.setTitle("Notifications History service");
+		alertDialog2.setTitle("Notifications Spy service");
 
 		// Setting Dialog Message
 		alertDialog2.setMessage(R.string.service_warning);
